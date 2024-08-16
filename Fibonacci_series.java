105121e946a1efffed908264ba780f1ea0f35dff
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-08-16 21:14:47 +0530

    Add files via upload

---
+++ Fibonacci_series.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class a7 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the number how much Fibonacci series you want to print");
+        int num = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+        System.err.println(a);
+        int c;
+        for (int i = 0; i < num; i++) {
+            c = a + b;
+            System.err.println(c);
+            a = b;
+            b = c;
+
+        }
+    }
+}
