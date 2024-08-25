6168b4c72db44043635fb832dbd1d0b31d0a64c1
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-08-25 23:59:17 +0530

    Add files via upload

---
+++ swap_by_BitwiseOperator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class swap_by_BitwiseOperator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers :");
+        int  a = sc.nextInt();
+        int  b = sc.nextInt();
+        System.out.println("a ="+a);
+        System.out.println("b =" + b);
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("After swap \n" +"a= "+ a +"\n" + "b="+ b);
+
+
+        
+
+
+        
+
+    }
+
+}
