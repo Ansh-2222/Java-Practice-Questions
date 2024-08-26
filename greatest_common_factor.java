30eca1244d8c4dafba430595a5c40b2e5200e125
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-08-26 23:22:47 +0530

    Add files via upload

---
+++ greatest_common_factor.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class greatest_common_factor {
+public static void main(String[] args) {
+    
+    Scanner sc= new Scanner(System.in);
+    System.err.println("Enter two number to find Greatest Common Divisor");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+     
+while (b!=0) 
+{
+    int r = a%b;
+    a=b;
+    b=r;
+    
+}
+System.out.println("The GCD is " + a);
+
+     
+
+}    
+}
