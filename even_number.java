ffa0248792af2bfa82a0902db0ad890045fe2455
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-08-28 23:26:21 +0530

    Add files via upload

---
+++ even_number.java
@@ -0,0 +1,14 @@
+
+public class even_number {
+    public static void main(String[] args) {
+
+        System.out.println(" Displaying Even Numbers From 1 to 100 ");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+
+        }
+    }
+
+}
