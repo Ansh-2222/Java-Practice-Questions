6edc50263bad9cc17e9a21017d1ef98f2e78b173
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-09-05 23:45:15 +0530

    Add files via upload

---
+++ array_Input.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+
+public class array_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int num = sc.nextInt();
+        System.out.println("Enter number to store in array");
+
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The array is ");
+        for (int i = 0; i < num; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
