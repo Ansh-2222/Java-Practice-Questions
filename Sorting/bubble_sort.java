1e68e3b911394d3feb0ac532363d58d6b4f3f6a4
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-10-10 00:15:04 +0530

    Add files via upload

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,28 @@
+
+public class bubble_sort {
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[] { 1, 2, 5, 2, 2, 3, 1 };
+
+       for(int i=0;i<arr.length-1;i++){
+       for(int j=0;j<arr.length-i-1;j++){
+
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        
+        }
+       }
+
+       for(int i=0;i<arr.length;i++)
+       {
+            System.out.print(arr[i]);
+
+       }
+        
+    }
+}
