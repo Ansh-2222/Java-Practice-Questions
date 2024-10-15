d544459caa8d23f43e21029d607062efda348219
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-10-15 15:07:33 +0530

    Add files via upload

---
+++ java-basic/input_output.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class input_output {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of x ");
+        int x = sc.nextInt();
+
+        System.out.println("value of x is " + x);
+    }
+}
